4252feed94f65e0768fbcd97722e3d4b3e7960d9
lumen <lumen2004.scholar@gmail.com>
2024-11-19 14:42:40 +0800

    feat (flow): add sampling_steps.

--- rectified_flow/data.py
+++ rectified_flow/data.py
@@ -8,16 +8,17 @@ from PIL import Image
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
-from typing import Tuple
+from typing import Tuple, Optional
 
 
 class DatasetForRectifiedFlow(ImageFolder):
-    def __init__(self, noise_cache_dir: str, *args, **kwargs):
+    def __init__(self, noise_cache_dir: str, sampling_steps: Optional[int]=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         num_samples = len(self.samples)
         if not self.detect_cache_dir(num_samples, noise_cache_dir):
             self.generate_noises(num_samples, noise_cache_dir)
         self.noise_cache_dir = noise_cache_dir
+        self.sampling_steps = sampling_steps
     
     def detect_cache_dir(self, num: int, cache_dir: str) -> bool:
         if not os.path.exists(cache_dir):
@@ -40,21 +41,24 @@ class DatasetForRectifiedFlow(ImageFolder):
     
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, float, int, torch.Tensor]:
         sample, target = super().__getitem__(index)
-        time_step = random.random()
+        time_step = random.random() if self.sampling_steps is None else random.randint(0, self.sampling_steps - 1) / self.sampling_steps
         noise = self.load_noise(index)
         input_image = sample * time_step + noise * (1 - time_step)
         return input_image, time_step, target, (sample - noise)
 
 
-def get_dataloader(noise_cache_dir: str, batch_size: int, shuffle: bool=False) -> DataLoader:
+def get_dataloader(noise_cache_dir: str, batch_size: int, shuffle: bool=False, sampling_steps: Optional[int]=None) -> DataLoader:
     """Load the dataloader.
     Args:
-        noise_cache_dir: the path to the cache of the noise images.
+        noise_cache_dir (str): the path to the cache of the noise images.
+        batch_size (int):
+        shuffle (bool, Optional): default to False.
+        sampling_steps (int, Optional): default to None; when assigned with an integer, it's the number of sampling steps during generation; it controls the time steps during training.
     """
     # The training images are black-white 3x255x255 images
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Lambda(lambda x: torch.nn.functional.pad(x[:1], (0, 1, 0, 1)))
     ])
-    data = DatasetForRectifiedFlow(noise_cache_dir, 'subclass12', transform=transform)
+    data = DatasetForRectifiedFlow(noise_cache_dir, sampling_steps, 'subclass12', transform=transform)
     return DataLoader(data, batch_size=batch_size, shuffle=shuffle)
