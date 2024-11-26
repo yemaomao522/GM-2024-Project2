c0d1b54a0b599712902be5ed11c4ce76d1851e4d
lumen <lumen2004.scholar@gmail.com>
2024-11-26 11:40:37 +0800

    fix (flow): fix ViT

--- rectified_flow/models/vit.py
+++ rectified_flow/models/vit.py
@@ -8,14 +8,14 @@ from torch import nn, Tensor, LongTensor
 class PatchEmbedding(nn.Module):
     """Patching and adding positional embeddings.
     """
-    def __init__(self, hidden_dim: int, img_size: int):
+    def __init__(self, hidden_dim: int, img_size: int, patch_size: int):
         super().__init__()
         self.proj = nn.Sequential(
-            nn.Conv2d(3, hidden_dim, kernel_size=4, stride=4),
+            nn.Conv2d(1, hidden_dim, kernel_size=patch_size, stride=patch_size),
             Rearrange("b c h w -> b (h w) c")
         )
         # self.cls_token = nn.Parameter(torch.randn((1, 1, hidden_dim), dtype=torch.float32), requires_grad=True)
-        self.pos_embed = nn.Parameter(torch.randn((1, (img_size // 4) ** 2, hidden_dim)))
+        self.pos_embed = nn.Parameter(torch.randn((1, (img_size // patch_size) ** 2, hidden_dim)))
         self.hidden_dim = hidden_dim
 
     def sin_cos_embedding(self, x: Tensor, dim: int):
@@ -34,23 +34,26 @@ class PatchEmbedding(nn.Module):
         # cls_token = self.cls_token.expand((x.shape[0], -1, -1))
         # z = torch.concat((cls_token, z), dim=1)
         pos_embed = self.pos_embed.expand(z.shape)
-        t_embed = self.sin_cos_embedding(t, self.hidden_dim)
-        y_embed = self.sin_cos_embedding(y + 1, self.hidden_dim)
+        t_embed = self.sin_cos_embedding(t, self.hidden_dim)[:, None, :].expand(z.shape)
+        y_embed = self.sin_cos_embedding(y + 1, self.hidden_dim)[:, None, :].expand(z.shape)
         return z + pos_embed + t_embed + y_embed
 
 
 class DecodingLayer(nn.Module):
-    def __init__(self, hidden_dim: int):
+    def __init__(self, hidden_dim: int, img_size: int, log_patch_size: int):
         super().__init__()
+        patch_size = 2 ** log_patch_size
         self.conv = nn.Sequential(
-            Rearrange("b (h w) d -> b h w d", h=4, w=4),
-            nn.ConvTranspose2d(hidden_dim, hidden_dim, kernel_size=2, stride=2),
-            nn.BatchNorm2d(hidden_dim),
-            nn.ReLU(),
-            nn.ConvTranspose2d(hidden_dim, hidden_dim, kernel_size=2, stride=2),
-            nn.BatchNorm2d(hidden_dim),
-            nn.ReLU(),
+            Rearrange("b (h w) d -> b d h w", h=img_size // patch_size, w=img_size // patch_size),
+            *[self.construct_upsampling(hidden_dim) for _ in range(log_patch_size)],
             nn.Conv2d(hidden_dim, 1, kernel_size=1)
+        )
+    
+    def construct_upsampling(self, hidden_dim: int):
+        return nn.Sequential(
+            nn.ConvTranspose2d(hidden_dim, hidden_dim, kernel_size=2, stride=2),
+            nn.LayerNorm(),
+            nn.ReLU()
         )
     
     def forward(self, x: Tensor):
@@ -58,14 +61,12 @@ class DecodingLayer(nn.Module):
 
 
 class TimeConditionalViT(VectorField):
-    """Fixed to 4x4 patch; accept 1xHxW images.
-    For convenience, H and W should be the same and divided by 4.
-    """
-    def __init__(self, hidden_dim: int, nhead: int, num_layers: int, img_size: int):
+    def __init__(self, hidden_dim: int, nhead: int, num_layers: int, img_size: int, log_patch_size: int):
         super().__init__()
-        self.patching = PatchEmbedding(hidden_dim, img_size)
+        patch_size = 2 ** log_patch_size
+        self.patching = PatchEmbedding(hidden_dim, img_size, patch_size)
         self.encoding = self.construct_encoder(hidden_dim, nhead, hidden_dim * 4, num_layers)
-        self.decoding = DecodingLayer(hidden_dim)
+        self.decoding = DecodingLayer(hidden_dim, img_size, log_patch_size)
     
     def construct_encoder(self, hidden_dim: int, nhead: int, dim_feedforward: int, num_layers: int):
         layer = nn.TransformerEncoderLayer(hidden_dim, nhead, dim_feedforward, norm_first=True, batch_first=True)
